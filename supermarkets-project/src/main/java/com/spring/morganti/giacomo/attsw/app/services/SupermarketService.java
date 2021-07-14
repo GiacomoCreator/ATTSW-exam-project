b889ce446b017b58ae6fea5b28f8daba601d4196
GiacomoCreator <giacomo.morganti@stud.unifi.it>
2021-07-14 19:38:37 +0200

    Added REST Controller with a temporary implementation of the Service.

---
+++ supermarkets-project/src/main/java/com/spring/morganti/giacomo/attsw/app/services/SupermarketService.java
@@ -0,0 +1,39 @@
+package com.spring.morganti.giacomo.attsw.app.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.spring.morganti.giacomo.attsw.app.model.Supermarket;
+
+@Service
+public class SupermarketService {
+
+	private static final String TEMPORARY_IMPLEMENTATION = "Temporary implementation";
+	
+	public Supermarket getSupermarketById(long l) {
+		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+	}
+
+	public List<Supermarket> getAllSupermarkets() {
+		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+	}
+
+	public Supermarket insertNewSupermarket(Supermarket newSupermarket) {
+		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+	}
+
+	public Supermarket updateSupermarketById(long id, Supermarket updatedSupermarket) {
+		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+	}
+
+	public void delete(Supermarket supermarketToDelete) {
+		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+		
+	}
+
+	public void deleteAllSupermarkets() {
+		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+	}
+
+}
