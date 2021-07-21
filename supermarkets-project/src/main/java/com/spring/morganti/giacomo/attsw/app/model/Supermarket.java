2d43b68f0c5cd55aba4b140791acd5644898c0a9
GiacomoCreator <giacomo.morganti@stud.unifi.it>
2021-07-21 15:31:28 +0200

    Added Web layer

--- supermarkets-project/src/main/java/com/spring/morganti/giacomo/attsw/app/model/Supermarket.java
+++ supermarkets-project/src/main/java/com/spring/morganti/giacomo/attsw/app/model/Supermarket.java
@@ -14,6 +14,10 @@ public class Supermarket {
 			this.address = address;
 		}
 
+		public Supermarket() {
+			// TODO Auto-generated constructor stub
+		}
+
 		public Long getId() {
 			return id;
 		}
