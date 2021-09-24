512c3765e5be99c4a81816afee43fa25ee14ee30
GiacomoCreator <giacomo.morganti@stud.unifi.it>
2021-09-24 12:35:44 +0200

    Fixed some minor warnings related to Groovy and Cucumber
    - specified Groovy version 3.0.4
    - specified Cucumber version 5.7.0
    - specified @CucumberOptions(strict = true) option into **BDD classes
    - used @CucumberContextConfiguration annotation into
    SupermarketsWebAppSteps class

--- supermarkets-project/src/e2e/java/com/spring/morganti/giacomo/attsw/app/e2e/SupermarketsWebAppSearchBDD.java
+++ supermarkets-project/src/e2e/java/com/spring/morganti/giacomo/attsw/app/e2e/SupermarketsWebAppSearchBDD.java
@@ -3,12 +3,12 @@ package com.spring.morganti.giacomo.attsw.app.e2e;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
-import cucumber.api.junit.Cucumber;
-import cucumber.api.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/e2e/resources/webApp_search.feature")
+@CucumberOptions(features = "src/e2e/resources/webApp_search.feature", strict = true)
 public class SupermarketsWebAppSearchBDD {
 	
 	@BeforeClass
--- supermarkets-project/src/e2e/java/com/spring/morganti/giacomo/attsw/app/e2e/steps/SupermarketsWebAppSteps.java
+++ supermarkets-project/src/e2e/java/com/spring/morganti/giacomo/attsw/app/e2e/steps/SupermarketsWebAppSteps.java
@@ -12,13 +12,15 @@ import org.springframework.boot.web.server.LocalServerPort;
 
 import com.spring.morganti.giacomo.attsw.app.repositories.SupermarketRepository;
 
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.spring.CucumberContextConfiguration;
 
+@CucumberContextConfiguration
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = "spring.main.allow-bean-definition-overriding=true")
 public class SupermarketsWebAppSteps {
 	
